09e2a4f389850cf233eb3503314f1e82557ba3a7
Richard L. Sypert Jr <richard@analyticowl.com>
2020-05-17 15:13:36 -0700

    update hosted sites and git repos links

--- fuel/application/views/webTech.php
+++ fuel/application/views/webTech.php
@@ -6,10 +6,10 @@
 		
 		<h1>Integrated Technologies</h1>
 		<p>This site shows my experience as a Software Developer, Technical Writer, and Engineer.  I have recent working experience with PHP and node.js on the backend
-			and Vue.js and Semantic UI on the front end.  This site is hosted on Digital Ocean Droplets.
+			and Vue.js and Semantic UI on the front end.  This site is hosted on Digital Ocean Droplets. Here is the <a href="http://159.65.100.7/fuel2/index.php/front">Website</a> and <a href="https://github.com/Rsypertjr/fuelCMS/tree/fuelPF">Git Repo</a>
 		</p>
 		<p>One droplet is Ubuntu 16 with nginx server utilizing Express.js/Node.js application framework.  It has an application using Pug templating, Bootstrap, 
-		   Mongo/Mongoose document-based database, Nodemon, Pm2, NPM, and other technologies. 
+		   Mongo/Mongoose document-based database, Nodemon, Pm2, NPM, and other technologies. Here is the <a href="http://206.189.211.36/">Website</a> and <a href="https://github.com/Rsypertjr/expressnode">Git Repo</a>
 		</p> 
 		<p>This site is mostly hosted on a Digital Ocean droplet containing Ubuntu 18.04 LAMP setup.  Fuel CMS framework, Bootstrap, JQuery, JQueryUI, SVG, XML, CSS are the main 
 		   technologies uses.  Some Vue.js can also be found.
