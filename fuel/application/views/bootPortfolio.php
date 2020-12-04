a43cf2020291b42959df331aae0fb90605e2afbb
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2020-12-04 11:39:31 -0800

    add description to voteparser

--- fuel/application/views/bootPortfolio.php
+++ fuel/application/views/bootPortfolio.php
@@ -1078,7 +1078,8 @@
 									</p>
 								</a>
 								<object data="voteparser" height="200" width="300"></object>
-								<p>See Tabular and Graphic Parser of 2020 Presidential Election Voting
+								<p>See Tabular and Graphic Parser of 2020 Presidential Election Voting. uses Vue.js, JQuery, Bootstrap, Datatables, and JChartfx, on the front-end, and PHP on the backend 
+									to create a dashboard and charts of elections data for all states. It pulls actual election data from the New York Times website and parses the JSON formatted data.   
 								   <br>The Code for PHP-based apps is here: <a href="https://github.com/Rsypertjr/fuelCMS/tree/fuelPF/fuel/application" target="_blank">PHP Code</a>
 								</p>
 							 </li>
