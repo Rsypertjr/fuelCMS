03509ea6774bfb8fb892242c43ef7e2f31c90861
Richard L. Sypert Jr <rsypertjr@DESKTOP-5M18UID>
2025-01-19 10:22:10 -0800

    origin/portfolio-10-17-2024

--- fuel/application/config/config.php
+++ fuel/application/config/config.php
@@ -38,7 +38,7 @@ $config['base_url'] = 'http:/localhost';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
