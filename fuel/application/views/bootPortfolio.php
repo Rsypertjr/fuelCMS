c415ac271fb623494f6b76380780f2f37ef9833c
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2025-10-03 20:08:50 -0700

    added top carousel entry for ai vote analyzer

--- fuel/application/views/bootPortfolio.php
+++ fuel/application/views/bootPortfolio.php
@@ -159,6 +159,23 @@
 						<!-- Wrapper for slides -->
 						<div class="carousel-inner">
 							<div class="carousel-item active">
+								<h4>AI Developer & GitHub Copilot Specialist<br/>
+								Presidential Voting Analysis & Code Storage Platform</h4>
+								<p class="d-flex justify-content-center text-break">Comprehensive Next.js application with Supabase SQL integration featuring interactive electoral data analysis, real-time voting
+									trend visualization, and demographic insights. Incorporates GitHub Copilot-powered code management
+									with intelligent organization, automated tagging, search functionality, and collaborative development
+								</p>
+								<div class="card-body" style="background-color:lightgrey;width:60%;margin-left:20%">
+									<h3 class="card-title" style="background-color:#b3ccff;padding:0.5em;color:black">New Next.js-AI-CoPilot Developed Presidential Voting Analyzer</h3>
+									<p class="card-text"style="background-color:#b3ccff;padding:0.5em" >Supabase SQL Integration for Front-End Dynamic Interaction, with Server-Side Rendering for SEO.&nbsp;&nbsp;
+										GitHub Copilot AI is used for Code Management and Intelligent Code Generation.
+										<a href="https://codestorage.vercel.app" target="_blank" class="btn btn-primary">See App</a>&nbsp;&nbsp;and&nbsp;&nbsp;
+										<a href="https://github.com/Rsypertjr/code_storage/tree/presidentelect" target="_blank" class="btn btn-primary">See the code!</a>
+									</p>
+									
+								</div>
+							</div>				
+							<div class="carousel-item">
 								<h4>Professional Experience</h4> 
 								<p class="text-break">This site shows my experience as a Software Developer,&nbsp;Technical Writer,&nbsp;and Engineer.&nbsp;&nbsp;It has been updated to Bootstrap 4.0.&nbsp;&nbsp;
 									This carousel presents Information about my Work History.
@@ -231,6 +248,10 @@
 								<li data-target="#myJumboCarousel" data-slide-to="2"></li>
 								<li data-target="#myJumboCarousel" data-slide-to="3"></li>
 								<li data-target="#myJumboCarousel" data-slide-to="4"></li>
+								<li data-target="#myJumboCarousel" data-slide-to="5"></li>
+								<li data-target="#myJumboCarousel" data-slide-to="6"></li>
+								<li data-target="#myJumboCarousel" data-slide-to="7"></li>
+								<li data-target="#myJumboCarousel" data-slide-to="8"></li>
 							</ol> 
 						</div>
 						<div>
