001af5ee4e0b5a8d22d7a526fa88f6ea77488405
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2025-10-04 00:51:57 +0000

    up-to-date 10/3/2025

--- fuel/application/views/bootPortfolio.php
+++ fuel/application/views/bootPortfolio.php
@@ -202,7 +202,7 @@
 							</div>
 							<div class="carousel-item">
 								<h4>Vue3 Composition API, Vue3/Bootstrap/JQuery/ChartJs Component-based front end</h4> 
-								<p>Vue 3 Composition API,&nbsp;Chart.js,&nbsp;and Bootstrap is used in this app:&nbsp;<a href="http://vuevotes.rsypertjr.net" 
+								<p>Vue 3 Composition API,&nbsp;Chart.js,&nbsp;and Bootstrap is used in this app:&nbsp;<a href="https://vue3chartjs.rsypertjr.net/" 
 								target="_blank">See App</a>.&nbsp;&nbsp;This app uses Vue3 Composition API which allows more function-based writing of components,&nbsp;inspired by React Hooks.&nbsp;&nbsp;
 								Here is code link:&nbsp;<a href="https://github.com/Rsypertjr/Vue3-Chartjs-Bootstrap.git" target="_blank">See Code</a>.
 								</p>
